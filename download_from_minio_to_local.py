044933f2e785418078ed75f65aab9c7eb1ff9cd1
unknown <wangyan@xiaobing.ai>
2022-08-05 11:11:08 +0800

    second

--- download_from_minio_to_local.py
+++ download_from_minio_to_local.py
@@ -29,13 +29,14 @@ def download_directory(
 
 
 if __name__ == '__main__':
-    path = '0/0d47d35713ef483ebd245be79c516719/artifacts'
-    # download_directory(bucket_name='mlflow', path=path, access_key='minioadmin', secret_key='minioadmin',
-    #                    endpoint_url='http://39.105.6.98:43098')
-    # mlflow.projects.run(uri=path + '/model', env_manager='conda')
-    project_uri = "https://github.com/mlflow/mlflow-example"
-    params = {"alpha": 0.5, "l1_ratio": 0.01}
+    path = '0/072084f402a942bf8dad8b4708502c08/artifacts'
+    download_directory(bucket_name='mlflow', path=path, access_key='minioadmin', secret_key='minioadmin',
+                       endpoint_url='http://39.105.6.98:43098')
+    mlflow.projects.run(uri=path + '/model', env_manager='conda')
+    mlflow.projects.SubmittedRun
+    # project_uri = "https://github.com/mlflow/mlflow-example"
+    # params = {"alpha": 0.5, "l1_ratio": 0.01}
 
     # Run MLflow project and create a reproducible conda environment
     # on a local host
-    mlflow.run(project_uri, parameters=params)
+    # mlflow.run(project_uri, parameters=params)
--- upload_test/predict.py
+++ upload_test/predict.py
@@ -1,1 +1,22 @@
-print('hellow mlflow')
+import argparse
+import json
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="you should add those parameter")
+    parser.add_argument('--config', type=str, default="./config.json", help="The path of config file")
+    parser.add_argument('--input', type=str, help='please input data which you want to predict')
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == '__main__':
+    arguments = parse_args()
+    print(arguments.config)
+    models = []
+    with open(arguments.config, encoding='utf-8', mode='r') as f:
+        models = json.load(f)
+    model_path = models['model_path']
+    print(model_path)
+    print('hellow mlflow')
